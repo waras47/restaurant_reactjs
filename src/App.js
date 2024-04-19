0a070a72cfc09d138eec1d6fd68ae6da023fc22f
marwansaputro <marwansaputro26@gmail.com>
2024-04-19 22:41:14 +0700

    add submenu

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom'; // Remove Switch import
 import Home from './home/Home';
-import Menu from './Menu/Menu';
+import Menu from './menus/Menu';
 
 const App = () => {
   return (
     <div>
           <Routes>
             <Route path="/" exact element={<Home />} />
-            <Route path="/menu" element={<Menu />} />
+            <Route path="/menu" exact element={<Menu />} />
+           
+     
            
           </Routes>
       </div>
