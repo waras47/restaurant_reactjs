ddb0b310e4b4ba7320c3be2ac3494866a4310a95
wahyudin470717 <wudin2692@gmail.com>
2024-05-04 11:58:26 +0700

    make translate to language english ang dutch , just in about us

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {
-  BrowserRouter,
-} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
+import './i18n/i18n';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
